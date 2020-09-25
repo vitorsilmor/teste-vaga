18751503ffae50570a9fbd9abe8bf2eca62ed761
Vitor Morais <vitorsm@id.uff.br>
2020-09-25 18:01:16 -0300

    Criação da base do layout

---
+++ resources/header.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>santri</title>
+
+    <link rel="stylesheet" href="<?php url(); ?>/static/css/w3.css">
+    <link rel="stylesheet" href="<?php url(); ?>/static/css/santri.css">
+    <link rel="stylesheet" href="<?php url(); ?>/static/css/toastr.css">
+
+    <link rel="stylesheet" href="<?php url(); ?>/static/css-awesome/brands.css">
+    <link rel="stylesheet" href="<?php url(); ?>/static/css-awesome/fontawesome.css">
+    <link rel="stylesheet" href="<?php url(); ?>/static/css-awesome/regular.css">
+    <link rel="stylesheet" href="<?php url(); ?>/static/css-awesome/solid.css">
+    <link rel="stylesheet" href="<?php url(); ?>/static/css-awesome/svg-with-js.css">
+    <link rel="stylesheet" href="<?php url(); ?>/static/css-awesome/v4-shims.css">
+
+    <style>
+      table {
+        border-collapse: collapse;
+        width: 100%;
+      }
+
+      th, td {
+        text-align: left;
+        padding: 8px;
+        border-bottom: 1px solid #ddd;
+      }
+
+      tr:nth-child(even) {background-color: #f2f2f2;}
+    </style>
+  </head>
+  <body>
+    <section class="row">
+      <div class="container">
