fca618164a7d3bb626ffa95e666e68712717174e
Vitor Morais <vitorsm@id.uff.br>
2020-09-25 18:03:05 -0300

    Rotas recebendo a model

--- config/route.php
+++ config/route.php
@@ -4,11 +4,15 @@ use App\Http\Route;
 
 $routes = new Route();
 
-$routes->get('/usuarios',                'UsuarioController',   'index');
-$routes->get('/usuarios/criar',          'UsuarioController',   'create');
-$routes->get('/usuarios/{id}/atualizar', 'UsuarioController',   'update');
+$routes->get('/usuarios',                'UsuarioController',   'index',  'Usuario');
+$routes->get('/usuarios/criar',          'UsuarioController',   'create', 'Usuario');
+$routes->get('/usuarios/{id}/atualizar', 'UsuarioController',   'update', 'Usuario');
 $routes->post('/usuarios',               'UsuarioController',   'store');
 $routes->put('/usuarios/{id}',           'UsuarioController',   'update');
 $routes->delete('/usuarios/{id}',        'UsuarioController',   'delete');
+
+$routes->get('/login',  'DashboardController', 'login',   'Usuario');
+$routes->post('/login', 'DashboardController', 'doLogin', 'Usuario');
+$routes->post('/logout','DashboardController', 'logout',  'Usuario');
 
 return $routes;
