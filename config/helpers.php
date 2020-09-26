986cce0288d087f49465d28bd0e63f626e149a6b
Vitor Morais <vitorsm@id.uff.br>
2020-09-26 12:03:27 -0300

    Inclusão de helpers de form e modal

--- config/helpers.php
+++ config/helpers.php
@@ -35,3 +35,25 @@ function redirect(?string $route = null)
 {
     header('Location: ' . url() . $route);
 }
+
+/**
+ * Insere na página o verificador de método da requisição.
+ *
+ * @param string $method
+ * @return string
+ */
+function form_method(string $method): string
+{
+    return  "<input type='text' class='hidden' name='method' value='{$method}'>";
+}
+
+
+/**
+ * Exibe o modal conforme a string.
+ *
+ * @return void
+ */
+function call_modal(string $modal,  $data = null): void
+{
+    view($modal, $data);
+}
