867e7f9936c7802f14a4c2d806e020e11caedff9
Vitor Morais <vitorsm@id.uff.br>
2020-09-26 12:03:47 -0300

    Atualização de rotas

--- config/route.php
+++ config/route.php
@@ -10,10 +10,12 @@ $routes->get('/usuarios/criar',          'UsuarioController',   'create', 'Usuar
 $routes->get('/usuarios/(\d)/atualizar', 'UsuarioController',   'update', 'Usuario', 'LoginMiddleware');
 $routes->post('/usuarios',               'UsuarioController',   'store',  'Usuario', 'LoginMiddleware');
 $routes->put('/usuarios/(\d)',           'UsuarioController',   'update', 'Usuario', 'LoginMiddleware');
-$routes->delete('/usuarios/(\d)',        'UsuarioController',   'delete', 'Usuario', 'LoginMiddleware');
+$routes->delete('/usuarios',             'UsuarioController',   'delete', 'Usuario', 'LoginMiddleware');
 
 $routes->get('/',       'DashboardController', 'login');
 $routes->post('/login', 'DashboardController', 'doLogin', 'Usuario');
 $routes->post('/logout','DashboardController', 'logout',  'Usuario', 'LoginMiddleware');
+
+$routes->get('/usuarios/index/json/data', 'JsonController', 'index', 'Usuario', 'LoginMiddleware');
 
 return $routes;
