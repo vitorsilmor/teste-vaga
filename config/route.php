182da349dcad19a225e825e5dac705e5a5640987
Vitor Morais <vitorsm@id.uff.br>
2020-09-26 04:19:33 -0300

    Uso do middleware e ajustes em algumas rotas

--- config/route.php
+++ config/route.php
@@ -4,15 +4,15 @@ use App\Http\Route;
 
 $routes = new Route();
 
-$routes->get('/usuarios',                'UsuarioController',   'index',  'Usuario');
-$routes->get('/usuarios/criar',          'UsuarioController',   'create', 'Usuario');
-$routes->get('/usuarios/{id}/atualizar', 'UsuarioController',   'update', 'Usuario');
-$routes->post('/usuarios',               'UsuarioController',   'store');
-$routes->put('/usuarios/{id}',           'UsuarioController',   'update');
-$routes->delete('/usuarios/{id}',        'UsuarioController',   'delete');
+$routes->get('/usuarios',                'UsuarioController',   'index',  'Usuario', 'LoginMiddleware');
+$routes->get('/usuarios/criar',          'UsuarioController',   'create', 'Usuario', 'LoginMiddleware');
+$routes->get('/usuarios/{id}/atualizar', 'UsuarioController',   'update', 'Usuario', 'LoginMiddleware');
+$routes->post('/usuarios',               'UsuarioController',   'store',  'Usuario', 'LoginMiddleware');
+$routes->put('/usuarios/{id}',           'UsuarioController',   'update', 'Usuario', 'LoginMiddleware');
+$routes->delete('/usuarios/{id}',        'UsuarioController',   'delete', 'Usuario', 'LoginMiddleware');
 
-$routes->get('/login',  'DashboardController', 'login',   'Usuario');
+$routes->get('/',       'DashboardController', 'login');
 $routes->post('/login', 'DashboardController', 'doLogin', 'Usuario');
-$routes->post('/logout','DashboardController', 'logout',  'Usuario');
+$routes->post('/logout','DashboardController', 'logout',  'Usuario', 'LoginMiddleware');
 
 return $routes;
