abec5302afcd0e778738ec7d9d7a1b159ab6aede
Vitor Morais <vitorsm@id.uff.br>
2020-09-26 04:13:22 -0300

    Implementação do controlle de login

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,25 +2,60 @@
 
 namespace App\Http\Controllers;
 
-class DashboardController
+use App\Http\Controllers\Abstracts\Controller;
+use App\Http\Request;
+
+class DashboardController extends Controller
 {
     /**
-     * Undocumented function
+     * Exibe uma view para o usuário fazer login.
      *
      * @return void
      */
-    public function index()
+    public function login(Request $request)
     {
-
+        view('dashboard.login');
     }
 
     /**
-     * Undocumented function
+     * Rota que executa o login.
      *
      * @return void
      */
-    public function login()
+    public function doLogin(Request $request)
     {
+        try {
+            session_start();
 
+            $usuario = $this->model->checkLogin(
+                $request->data->user,
+                $request->data->pass
+            );
+    
+          
+            if(isset($usuario->USUARIO_ID)){
+                $_SESSION['USUARIO_ID']    = $usuario->USUARIO_ID;
+                $_SESSION['NOME_COMPLETO'] = $usuario->NOME_COMPLETO;
+                $_SESSION['LOGIN']         = $usuario->LOGIN;
+    
+                return redirect('/usuarios');
+            }
+        } catch (\Throwable $th) {
+            redirect("/?error=true");
+        }
+        
+    }
+
+    /**
+     * Rota que executa o logout.
+     *
+     * @return void
+     */
+    public function logout(Request $request)
+    {
+        session_start();
+        session_destroy();
+
+        redirect();
     }
 }
