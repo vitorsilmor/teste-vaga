32afd4efd2631682f6e33e917c263b8d9c4d2e2a
Vitor Morais <vitorsm@id.uff.br>
2020-09-26 04:19:00 -0300

    Inclusão do helper de redirecionamento

--- config/helpers.php
+++ config/helpers.php
@@ -2,14 +2,36 @@
 
 use App\Views\UsuarioView;
 
-function view( string $path, $data = null)
+/**
+ * Helper que renderiza a view.
+ *
+ * @param string $path
+ * @param mixed $data
+ * @return void
+ */
+function view( string $path, $data = null): void
 {
     $view = new UsuarioView();
     $view->render($path, $data);
 }
 
-function url()
+/**
+ * Helper que retorna a url.
+ *
+ * @return string
+ */
+function url(): string
 {
-
     return "http://" . $_SERVER['HTTP_HOST'];
+}
+
+/**
+ * Helper de redirecionamento.
+ *
+ * @param string $route
+ * @return void
+ */
+function redirect(?string $route = null)
+{
+    header('Location: ' . url() . $route);
 }
