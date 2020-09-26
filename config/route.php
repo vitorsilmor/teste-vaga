a912db2cc8741f45794b928d922058d20a9d0b8b
Vitor Morais <vitorsm@id.uff.br>
2020-09-26 06:40:11 -0300

    Redefinição das rotas

--- config/route.php
+++ config/route.php
@@ -5,11 +5,12 @@ use App\Http\Route;
 $routes = new Route();
 
 $routes->get('/usuarios',                'UsuarioController',   'index',  'Usuario', 'LoginMiddleware');
+$routes->get('/usuarios/(\d)',           'UsuarioController',   'show',  'Usuario', 'LoginMiddleware');
 $routes->get('/usuarios/criar',          'UsuarioController',   'create', 'Usuario', 'LoginMiddleware');
-$routes->get('/usuarios/{id}/atualizar', 'UsuarioController',   'update', 'Usuario', 'LoginMiddleware');
+$routes->get('/usuarios/(\d)/atualizar', 'UsuarioController',   'update', 'Usuario', 'LoginMiddleware');
 $routes->post('/usuarios',               'UsuarioController',   'store',  'Usuario', 'LoginMiddleware');
-$routes->put('/usuarios/{id}',           'UsuarioController',   'update', 'Usuario', 'LoginMiddleware');
-$routes->delete('/usuarios/{id}',        'UsuarioController',   'delete', 'Usuario', 'LoginMiddleware');
+$routes->put('/usuarios/(\d)',           'UsuarioController',   'update', 'Usuario', 'LoginMiddleware');
+$routes->delete('/usuarios/(\d)',        'UsuarioController',   'delete', 'Usuario', 'LoginMiddleware');
 
 $routes->get('/',       'DashboardController', 'login');
 $routes->post('/login', 'DashboardController', 'doLogin', 'Usuario');
